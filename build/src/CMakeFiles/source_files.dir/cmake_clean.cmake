8b02c2586c8289fdf7b653c7a8c1913eb17eec38
NayrMu <123273762+NayrMu@users.noreply.github.com>
2023-12-21 14:56:49 -0600

    Hello World

---
+++ build/src/CMakeFiles/source_files.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/source_files.dir/main.cpp.obj"
+  "CMakeFiles/source_files.dir/main.cpp.obj.d"
+  "libsource_files.dll.a"
+  "source_files.exe"
+  "source_files.exe.manifest"
+  "source_files.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/source_files.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+    std::cout << "hello world!";
+    return 0;
+}
